bb6d23b104eb3c6b12818b3b996b21267ae9286b
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-09-28 15:15:54 +0530

    final

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var tablinks= document.getElementsByClassName('tab-links');
+// ABOUT PAGE TO SKILLS AND EXPERIENCE AND EDUCATION
+var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");   
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
     for (tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab")
@@ -10,3 +11,31 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+
+// SLIDE MENU FUNCTIONALITY
+var sidemenu = document.getElementById('sidemenu')
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+//CONTACT FORM GOOGLE SHEET
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxaktTXznbMwzMSsIyB6B_-5H7y2HEvFmCA9teOiH10Y9ETMWUanFTzPiVTam56SUFJUw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function (){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+}) 
